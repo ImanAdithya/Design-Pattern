70afd7b498a50db19aa7996bfc00524d8c43009c
ImanAdithya <adithyaiman07@gmail.com>
2023-11-10 20:44:15 +0530

    content updated

--- Observer_Design_Pattern/src/Main.java
+++ Observer_Design_Pattern/src/Main.java
@@ -1,5 +1,27 @@
- public class Main {
+import service.impl.Dasun;
+import service.impl.Kamal;
+import service.impl.Ramal;
+import service.impl.Sithum;
+
+public class Main {
     public static void main(String[] args) {
 
+        //Initialized the Subject
+        Kamal kamal=new Kamal ();
+
+        //Initialized th Observers
+        Dasun dasun=new Dasun ();
+        Ramal ramal=new Ramal ();
+        Sithum sithum=new Sithum ();
+
+        //register the Observers to subject
+        kamal.register (dasun);
+        kamal.register (ramal);
+        kamal.register (sithum);
+
+        //Now see what will happen after the subject state change
+        kamal.notifyAllObservers ("Kamal upload a New Photo");
+        kamal.notifyAllObservers ("Kamal is Dead");
+        kamal.notifyAllObservers ("Im in Dubai");
     }
 }
